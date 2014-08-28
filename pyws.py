03f7d118e95f7da21e2fe48c58bb8630a098c02b
Eizo <eizoassik@gmail.com>
2014-08-28 15:25:10 +0800

    combine literal and instructures

--- pyws.py
+++ pyws.py
@@ -2,14 +2,21 @@
 
 from wslexer import Reader, Lexer
 from style import STL
-from wsbuiltin import WSOperation
+from wsbuiltin import WSOperation, WSLiteral
 
 
-def wscompiler(src:str, style:dict=STL):
+def wscompiler(src: str, style: dict=STL, strict=False):
     ins = []
-    for token in Lexer(Reader(src, style)):
-        if issubclass(token, WSOperation):
-            ins.append(token())
+    ins_buff = None
+    for token in Lexer(Reader(src, style), strict):
+        if hasattr(token, 'ARGS'):
+            if token.ARGS == 0:
+                ins.append(token())
+            else:
+                ins_buff = token
+        if isinstance(token, WSLiteral):
+            ins.append(ins_buff(token))
+            ins_buff = None
     return ins
 
 

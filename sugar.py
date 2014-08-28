0156649be2360634c2641338778879f6ad3cdfdf
Eizo <eizoassik@gmail.com>
2014-08-28 23:00:22 +0800

    support PUSHS

---
+++ sugar.py
@@ -0,0 +1,33 @@
+# encoding=utf-8
+import re
+
+
+class SyntaxSugar(object):
+    NAME = "@"
+
+    @staticmethod
+    def expand(arg):
+        pass
+
+
+class PUSHS(SyntaxSugar):
+    NAME = "PUSHS"
+
+    @staticmethod
+    def expand(arg: str):
+        rev = False
+        arg = arg.strip()
+        if arg.startswith('~'):
+            arg = arg[1:]
+            rev = True
+        if re.match(r"\[ ?((-?\d+ ?, ?)( ?))*-?\d+ ?\]", arg):
+            ints = eval(arg)
+            if rev:
+                ints = ints[::-1]
+            return list(map(lambda i: 'PUSH {}'.format(i), ints))
+        if re.match(r"\".*\"", arg):
+            arg = arg.strip('\"')
+            if rev:
+                arg = arg[::-1]
+            return list(map(lambda i: 'PUSH \'{}'.format(i), arg))
+        raise SyntaxError("Cannot expand PUSHS {}".format(arg))

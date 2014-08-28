30d5b6c354cbc5238afc984c87891aa754b0c31d
Eizo <eizoassik@gmail.com>
2014-08-28 10:19:37 +0800

    +STL/ORIGIN

---
+++ style.py
@@ -0,0 +1,7 @@
+# encoding=utf-8
+"""
+Use these dicts to make pyws works on different notations of WhiteSpace.
+"""
+
+ORIGIN = {' ': 'S', '\t': 'T', '\n': 'L'}
+STL = {'S': 'S', 'T': 'T', 'L': 'L'}

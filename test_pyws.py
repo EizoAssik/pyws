e66abc3e50c0a595ea3f56b80202b4c8980c936f
Eizo <eizoassik@gmail.com>
2014-08-28 10:55:27 +0800

    add __ne__ for WSOperators

--- test_pyws.py
+++ test_pyws.py
@@ -1,8 +1,8 @@
 # from lexer import Lexer
 import style
-from wsbuiltin import ADD, PUSH
+from wsbuiltin import ADD, PUSH, SUB
 from wslexer import Lexer
-from pyws import wscompiler
+from pyws import wscompiler, disassembler
 
 
 def test_source():
@@ -16,8 +16,18 @@ def test_iter():
 
 
 def test_compiler():
-    assert [ADD] == wscompiler('TSSS')
+    assert [ADD()] == wscompiler('TSSS')
+    assert [ADD(), SUB()] == wscompiler('TSSSTSST')
 
 
 def test_repr():
     assert "PUSH 3" == repr(PUSH(3))
+
+
+def test_disassembler():
+    assert "ADDSUB" == disassembler(wscompiler("TSSSTSST"))
+    assert "ADD-SUB" == disassembler(wscompiler("TSSSTSST"), sep='-')
+
+
+if __name__ == '__main__':
+    test_compiler()

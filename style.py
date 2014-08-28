bdb82c1dfbd976e2cdbdafaaa82eadd9eade2900
Eizo <eizoassik@gmail.com>
2014-08-28 14:09:41 +0800

    add literal tests

--- style.py
+++ style.py
@@ -5,3 +5,4 @@ Use these dicts to make pyws works on different notations of WhiteSpace.
 
 ORIGIN = {' ': 'S', '\t': 'T', '\n': 'L'}
 STL = {'S': 'S', 'T': 'T', 'L': 'L'}
+GMH = {'草': 'S', '泥': 'T', '马': 'L'}
--- test_pyws.py
+++ test_pyws.py
@@ -1,6 +1,7 @@
 # from lexer import Lexer
 import style
 from wsbuiltin import ADD, PUSH, SUB
+import wsbuiltin
 from wslexer import Reader
 from pyws import wscompiler, disassembler
 
@@ -29,5 +30,13 @@ def test_disassembler():
     assert "ADD-SUB" == disassembler(wscompiler("TSSSTSST"), sep='-')
 
 
-if __name__ == '__main__':
-    test_compiler()
+def test_literal():
+    assert "<NUMBER 1001/-6>" == repr(wsbuiltin.NUMBER('TSST'))
+    assert "<NUMBER 01011/11>" == repr(wsbuiltin.NUMBER('STSTT'))
+    assert "<LABEL 1001/9>" == repr(wsbuiltin.LABEL('TSST'))
+    assert 3 == wsbuiltin.NUMBER("ST") + 2
+    assert 0 == 1 - wsbuiltin.NUMBER("ST")
+    assert 6 == wsbuiltin.NUMBER("STT") * wsbuiltin.NUMBER("STS")
+    assert 2 == 5 // wsbuiltin.NUMBER("STS")
+    assert 9 == 10 + wsbuiltin.NUMBER("TS")
+    assert 1 == 3 % wsbuiltin.NUMBER("STS")

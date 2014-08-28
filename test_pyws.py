f7dcd1d29a303441747599a42ae458a5d87b965f
Eizo <eizoassik@gmail.com>
2014-08-29 00:51:10 +0800

    add cases

--- test_pyws.py
+++ test_pyws.py
@@ -2,7 +2,7 @@
 import style
 from wsbuiltin import ADD, PUSH, SUB, NUMBER, LABEL
 from wslexer import Reader
-from pyws import compiler, disassembler, assembler
+from pyws import compiler, disassembler, assembler, wsfunction, wsmark
 from engine import PYWSEngine
 
 
@@ -47,17 +47,49 @@ def test_literal():
 
 
 def test_assembler():
-    assert "SS;STSTSL" == assembler("PUSH 10")
+    assert "SS;STSTSL" == assembler("PUSH 10")[0]
+    assert "SS;SSL;SS;STL" == assembler("PUSHS [0,1]")[0]
+
 
 def ws_run(src: str) -> ([], {}):
     return PYWSEngine(compiler(src)).run()
+
 
 def test_engine():
     # PUSH 01 ; PUSH 03 ; ADD
     assert ([4], {}) == ws_run("SSSTL;SSSTTL;TSSS")
     # PUSH 01 ; PUSH 02 ; STORE
-    assert ([], {NUMBER("ST"): NUMBER("STS")}) == ws_run("SSSTL;SSSTSL;TTS")
+    assert ([], {NUMBER("ST"): NUMBER("STS")}) == ws_run("SS;STL;SS;STSL;TTS")
     # PUSH 03 ; PUSH 01 ; STORE ; PUSH 03 ; RETRIEVE
-    assert ([NUMBER("ST")], {NUMBER("STT"): NUMBER("ST")}) == ws_run("SSSTTL;SSSTL;TTS;SSSTTL;TTT")
+    assert ([NUMBER("ST")], {NUMBER("STT"): NUMBER("ST")}) == ws_run(
+        "SSSTTL;SSSTL;TTS;SSSTTL;TTT")
     # PUSH 01 ; TOP-COPY ; STORE ;
-    assert ([NUMBER("STTSS")], {}) ==  ws_run('SS;STL;SS;STSL;TSSS;SLL;SS;STTL;SS;STSSL;TSSL')
+    # PUSH 01 ; PUSH 02 ; ADD ; POP ; PUSH 03 ; PUSH 04 ; MUL
+    assert ([NUMBER("STTSS")], {}) == ws_run(
+        'SS;STL;SS;STSL;TSSS;SLL;SS;STTL;SS;STSSL;TSSL')
+
+
+@wsfunction()
+def add(a, b):
+    """TSSS"""
+
+@wsmark("TT")
+@wsfunction()
+def wsadd(a, b):
+    """TSSS"""
+
+@wsmark("ST")
+def mul(a, b):
+    return a * b
+
+
+def test_wsXpy():
+    assert 3 == add(1, 2)
+    # PUSH 03 ; PUSH 02 ; PYFN 01
+    assert 6 == ws_run("SSSTTL;SSSTSL;LLSSTL")[0].pop()
+    # PUSH 03 ; PUSH 02 ; PYFN -1
+    assert 5 == ws_run("SSSTTL;SSSTSL;LLSTTL")[0].pop()
+
+
+if __name__ == '__main__':
+    test_assembler()

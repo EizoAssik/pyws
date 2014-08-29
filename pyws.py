1fad5cb33dface2345e29e9bafe8d9f84d7fc55a
Eizo <eizoassik@gmail.com>
2014-08-29 11:38:41 +0800

    add doc

--- pyws.py
+++ pyws.py
@@ -10,6 +10,9 @@ from wsbuiltin import WSLiteral, LABEL
 
 
 def compiler(src: str, style: dict=wsstyle.STL, strict=False):
+    """
+    Compile STL codes into callable WSOperations
+    """
     ins = []
     ins_buff = None
     for token in Lexer(Reader(src, style), strict):
@@ -25,10 +28,19 @@ def compiler(src: str, style: dict=wsstyle.STL, strict=False):
 
 
 def disassembler(ins, sep=''):
+    """
+    Give a list of callable WSOperators,
+    return the wsa notations, which can be read back again
+    by assembler
+    """
     return sep.join(map(repr, ins))
 
 
 def assembler(src, sep=';', arg_sep=';'):
+    """
+    Give a wsa file's path or a string contains wsa,
+    return 2 values, STL code and instructions
+    """
     a = Assembler(AssemblerReader(src), arg_sep=arg_sep)
     return sep.join(a.src), a.ins
 
@@ -74,6 +86,33 @@ def main():
 def wsfunction(style=wsstyle.STL, strict=False, debug=False,
                stack_only=False, heap_only=False, stack_heap=False,
                ret_top=True):
+    """
+    This decorator will make a callable function in Python, which is written in
+    WhiteSpace.
+
+    The flags are defined below:
+    :style code style, STL, ORIGIN, or GMH. default: STL
+    :strict if set to True, program must end at LLL. default: False
+    :debug if set to True, program will run in debug mode. default: False
+    :ret_top return the top of the stack after running the code. default: True
+    :stack_only only return the whole stack. default: False
+    :heap_only only return the whole heap. default: False
+    :stack_heap return 2 values as stack, heap. default False
+
+    This decorator ONLY cares 2 things: the amount of arguments, and the __doc__
+
+    The args will be push to the top of  stack one by one, and the __doc__
+    contains all the WhiteSpace code.
+
+    Usage:
+        @wsfunction(stack_only=True)
+        def swap_dup(x, y):
+            \"\"\"SLTSLS\"\"\"
+
+        dup_swap(1,2) # => [2, 1, 1]
+
+    See test_pyws.test_wsXpy for more cases.
+    """
     def _wsdef(func):
         src = func.__doc__
         ins = compiler(src, style, strict)
@@ -95,7 +134,25 @@ def wsfunction(style=wsstyle.STL, strict=False, debug=False,
     return _wsdef
 
 
-def wsmark(label):
+def wsmark(label: str):
+    """
+    This decorator will register given function to engine.PYFN_MAP
+    using key `lebel`, a LABLE object or a string to initialize a new LABEL
+
+    mark a function like:
+
+    @wsmark("ST")
+    def add(a, b):
+        return a + b
+
+    then call in WhiteSpace like:
+
+    LLS-STL
+
+    Where LLS is the PYFN instruction and ST is the label.
+
+    See test_pyws.test_wsXpy for more usage.
+    """
     l = LABEL(label)
 
     def _wsmark(func):
